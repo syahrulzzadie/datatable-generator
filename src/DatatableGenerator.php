bae48ee1ec6d5218aefed381db4e0f6d126734d0
Syahrul Adiguna Kurniawan <syahrulzzadie@gmail.com>
2023-01-23 11:28:20 +0700

    Update DatatableGenerator.php

--- src/DatatableGenerator.php
+++ src/DatatableGenerator.php
@@ -5,7 +5,7 @@ namespace syahrulzzadie\DatatableGenerator;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\App;
 
-class DatatablesGenerator
+class DatatableGenerator
 {
     protected static $draw = 0;
     protected static $index = false;
